89c0e9c17a4e0da467aabc992fc5538429111b27
GangblankWolf <spiderinkling@gmail.com>
2024-05-09 14:03:32 -0500

    Features added

--- src/start-page/StartPage.js
+++ src/start-page/StartPage.js
@@ -12,9 +12,16 @@ function StartPage() {
       </div>
       <div class="SearchDescription">
          <h1 id="Title">Search in your area</h1>
-         <p id="description1">Use the serach page by radius, area, age restrictions, event type, event price, date and time ranges. And allow you to pick the event you want to attend.</p>
+         <p>Use the serach page by radius, area, age restrictions, event type, event price, date and time ranges. And allow you to pick the event you want to attend.</p>
       </div>
-      <div class="CalenderDescription"></div>
+      <div class="CalenderDescription">
+         <h1 id='caltitle'>Manage your events schedule</h1>
+         <p>The Calender page lets you see and manage your events. See the area time and date, for the whole year.</p>
+      </div>
+      <div class='Events'>
+         <h1 id='EVTitle'>Personized List</h1>
+         <p>If instead of a calander you are able to see a list of your events. Manage your events, listed by date and time.</p>
+      </div>
       </div>
    )
 }
