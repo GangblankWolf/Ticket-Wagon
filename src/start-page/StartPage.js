d9e323e7902cd4407a2c3bb9c85d77b1149c5403
GangblankWolf <spiderinkling@gmail.com>
2024-05-24 10:45:31 -0500

    Finished starting page

--- src/start-page/StartPage.js
+++ src/start-page/StartPage.js
@@ -16,11 +16,12 @@ function StartPage() {
          <h1 id="Title">Search in your area</h1>
          <p>Use the serach page by radius, area, age restrictions, event type, event price, date and time ranges. And allow you to pick the event you want to attend.</p>
       </div>
-      <img src='/public/CalActionDelete.gif' alt='Calendar Action Delete'></img>
+
     <div class="CalendarDescription">
          <h1 id='caltitle'>Manage your events schedule</h1>
          <p id='calparacontainer'>The Calendar page lets you see and manage your events. See the area time and date, for the whole year with just one click! No need for the hard to use and uninteresting calendar, when you can have a simple, easy to use, no hassle calendar, with no extra fees. That's right it's free! and you can enjoy it by signing up and logging in. It's just that effortless!</p>
       </div>
+
       <div class='Events'>
          <h1 id='EVTitle'>Personized List</h1>
          <p>If instead of a calander you are able to see a list of your events. Manage your events, listed by date and time.</p>
